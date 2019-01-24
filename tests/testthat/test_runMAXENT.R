5991d31cf7e33f3235e7143ea51fb79a8e51b941
Valentina Grisales <valengsb@gmail.com>
2019-01-23 23:55:49 -0500

    Maxent test

--- tests/testthat/test_runMAXENT.R
+++ tests/testthat/test_runMAXENT.R
@@ -15,13 +15,12 @@ source("test_helper_functions.R")
 out.gbif <- c1_queryDb(spName = "panthera onca", occDb = "gbif", occNum = 100)
 occs <- as.data.frame(out.gbif$cleaned)
 
-records <- which(is.na(raster::extract(envs$bio1.1, occs[,3:4])) == TRUE)
-occs <- occs[-records, ] 
-
-
 ## background mask
 # enviromental data
 envs <- c3_worldclim(bcRes = 10, bcSel = (list(TRUE,TRUE,TRUE,TRUE,TRUE)))
+# remove records without enviromental values 
+records <- which(is.na(raster::extract(envs$bio1.1, occs[,3:4])) == TRUE)
+occs <- occs[-records, ] 
 # background extent 
 bgExt <- c4_bgExtent(occs, envs, bgSel = 'bb', bgBuf = 0.5) 
 # background masked 
@@ -38,51 +37,61 @@ occsGrp = partblock$occ.grp
 # background points partitioned
 bgGrp = partblock$bg.grp
 
-## regularization multipliers 
-rms <- c(1:5)
-## regularization multipliers step value
-rmsStep <- 1
-## feature classes
-fcs <- c('L')
 
-## algorithm
-maxentJar <- 'maxent.jar'
-maxnet <- 'maxnet'
-bioclim <- 'bioclim'
-
-
-### run function
-
-## Maxent.jar
-maxentjar <- runMaxent(occs, bg, occsGrp, bgGrp, bgMsk, rms, rmsStep, fcs, clampSel = TRUE, 
-                       algMaxent = maxentJar)
-
+### Test BIOCLIM
 
 ## test if the error messages appear when they are supposed to 
 test_that("error checks", {
   #
   expect_error(runMaxent(occs, bg, occsGrp = NULL, bgGrp, bgMsk, rms, rmsStep, fcs, 
                          clampSel = TRUE, algMaxent = 'maxent.jar'), "Before building a model, please partition 
-                        occurrences for cross-validation.")
+               occurrences for cross-validation.")
 })
+
+
+### Test MAXENT
+
+## regularization multipliers 
+rms <- c(1:2)
+## regularization multipliers step value
+rmsStep <- 1
+## feature classes
+fcs <- c('L', 'LQ', 'H', 'LQH', 'LQHP', 'LQHPT')
+
+## algorithm
+algoritm <- c('maxent.jar','maxnet')
 
 ### test output features 
 test_that("output type checks", {
-  # the output is a list
-  expect_is(maxentjar, "list")
-  expect_is(maxentjar[c("evalTbl","evalTblBins", "models")], "list")
-  expect_is(maxentjar$predictions, "RasterStack")
-  expect_is(maxentjar$occPredVals, "matrix")
-  # the list has two elements
-  expect_equal(length(maxentjar), 5)
-  expect_equal(length(maxentjar$models), (length(rms)/rmsStep)*length(fcs))
-  expect_equal(nrow(maxentjar$evalTbl), (length(rms)/rmsStep)*length(fcs))
-  expect_equal(nrow(maxentjar$evalTblBins), (length(rms)/rmsStep)*length(fcs))
-  expect_equal(ncol(maxentjar$evalTbl), nrow(maxentjar$evalTbl)*4)
-  expect_equal(ncol(maxentjar$evalTblBins), nrow(maxentjar$evalTblBins)*4)
-  expect_equal(length(maxentjar$models), raster::nlayers(maxentjar$occPredVals))
-})
+  i <- algoritm[1]
+  for (i in algoritm) { 
+    maxentAlg <- runMaxent(occs, bg, occsGrp, bgGrp, bgMsk, rms, rmsStep, fcs, clampSel = TRUE, 
+                           algMaxent = i)
+    # the output is a list
+    expect_is(maxentAlg, "list")
+    expect_is(maxentAlg[c("evalTbl","evalTblBins", "models")], "list")
+    expect_is(maxentAlg$predictions, "RasterStack")
+    expect_is(maxentAlg$occPredVals, "matrix")
+    # the list has two elements
+    expect_equal(length(maxentAlg), 5)
+    expect_equal(length(maxentAlg$models), (length(rms)/rmsStep)*length(fcs))
+    expect_equal(nrow(maxentAlg$evalTbl), (length(rms)/rmsStep)*length(fcs))
+    expect_equal(ncol(maxentAlg$evalTbl), 16)
+    expect_equal(nrow(maxentAlg$evalTblBins), (length(rms)/rmsStep)*length(fcs))
+    expect_equal(ncol(maxentAlg$evalTblBins), (nlevels(factor(occsGrp)))*4)
+    expect_equal(length(maxentAlg$models), raster::nlayers(maxentAlg$predictions))
+  }
+  })
 
+
+### run function
+
+## Maxent.jar
+#maxentjar <- runMaxent(occs, bg, occsGrp, bgGrp, bgMsk, rms, rmsStep, fcs, clampSel = TRUE, 
+                       #algMaxent = maxentJar)
+
+#maxnet <- runMaxent(occs, bg, occsGrp, bgGrp, bgMsk, rms, rmsStep, fcs, clampSel = TRUE, 
+                       #algMaxent = maxnet)
 
 
 #maxentjar$models$
